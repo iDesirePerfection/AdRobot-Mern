2c2b59fa86e1738c24a79d216cfe0001a939fdcd
mouhibBenRhouma <mouhib.benrhouma@esprit.tn>
2020-05-13 06:39:47 +0100

    targeted offers update
    list of targets by offer
    offers corresponding to the customer’s question

--- routes/api/search.js
+++ routes/api/search.js
@@ -9,30 +9,53 @@ var url = require('url');
 const Segmentation = require('./segmentation');
 var natural = require('natural');
 var tokenizer = new natural.WordTokenizer();
-// @route   GET api/customers
-// @desc    Get all customers
-// @access  PUBLIC 
+
+// @route   GET api/search
+// @desc    Get all offers matched 
+// @access  PRIVATE 
 router.get('/offers/:question', async (req, res) => {
-
-    console.log(req.params.question)
     var rep = req.params.question
+    var params = querystring.parse(url.parse(req.url).query);
+    const { priceMax } = params
+    let listTarget = [];
+    console.log(Segmentation(rep))
+    try {
+        const list = await Offer.find();
+        list.forEach(element => {
+            var rep2 = Segmentation(tokenizer.tokenize(element.description));
 
-    res.json(Segmentation(rep));
+            if (listTarget.indexOf(element) == -1) {
+                if (rep2 == Segmentation(rep) || Segmentation(rep) == "all offers")
+                    listTarget.push(element)
+
+            }
+        });
+
+        if (priceMax) {
+            listTarget.forEach(element => {
+                if (element.minimumPrice > priceMax)
+                    listTarget.splice(listTarget.indexOf(element));
+            });
+        }
+
+        res.json(listTarget)
 
 
-
-
+    } catch (error) {
+        console.error(error.messsage);
+        res.status(500).send('server error');
+    }
 
 
 });
 router.get('/fahd/:id_offre', async (req, res) => {
     try {
         const offer = await Offer.findById(req.params.id_offre);
-
+        console.log(offer)
         var list = tokenizer.tokenize(offer.description)
         var tab = [];
         console.log(list)
-       
+        console.log(Segmentation(offer.description))
         list.forEach(element => {
             var rep = Segmentation(element);
 
@@ -40,7 +63,7 @@ router.get('/fahd/:id_offre', async (req, res) => {
             if (tab.indexOf(rep) == -1)
                 tab.push(rep)
         });
-        res.json(obj)
+        res.json(tab)
     } catch (error) {
         console.error(error.message);
         res.status(500).send('server error');
